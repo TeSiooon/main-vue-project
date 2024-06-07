07182519e3aa78cf9922683f405b57470c4bd3f5
TeSiooon <piotr.kowalczyk9908@gmail.com>
2024-06-07 09:25:39 +0200

    fetching requests

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,57 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
+      // id: new Date().toISOString(),
+      // coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message,
     };
+    const res = await fetch(
+      `https://react-kurs-771f0-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      }
+    );
+    const resData = await res.json();
+    if (!res.ok) {
+      //oblsuga bledu
+      const error = new Error(resData.message || 'Failed to fethc');
+      throw error;
+    }
+    newRequest.id = resData.name;
+    newRequest.coachId = payload.coachId;
     context.commit('addRequest', newRequest);
+  },
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+    const res = await fetch(
+      `https://react-kurs-771f0-default-rtdb.firebaseio.com/requests/${coachId}.json`,
+      {
+        method: 'GET',
+      }
+    );
+
+    const resData = await res.json();
+
+    if (!res.ok) {
+      //oblsuga bledu
+      const error = new Error(resData.message || 'Failed to fetch requests');
+      throw error;
+    }
+
+    const requests = [];
+
+    for (const key in resData) {
+      const request = {
+        id: key,
+        coachId: coachId,
+        userEmail: resData[key].userEmail,
+        message: resData[key].message,
+      };
+
+      requests.push(request);
+    }
+    context.commit('setRequests', requests);
   },
 };
