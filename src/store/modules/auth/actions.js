2a1b36398874aedc89f9abf8d6b4c29442acacd2
TeSiooon <piotr.kowalczyk9908@gmail.com>
2024-06-10 10:34:07 +0200

    added auto login and auto logout

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,67 +1,100 @@
+let timer;
+
 export default {
   async login(context, payload) {
-    const res = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBBH2-PwTEqMNLKrrTINwbz0MS9yeRUq5s',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
-
-    const resData = await res.json();
-
-    if (!res.ok) {
-      //error
-      const error = new Error(resData.error.message || 'Failed');
-      console.log(error);
-      throw error;
-    }
-
-    console.log(resData);
-    context.commit('setUser', {
-      token: resData.idToken,
-      userId: resData.localId,
-      tokenExpiration: resData.expiresIn,
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
     });
   },
+
   async signup(context, payload) {
-    const res = await fetch(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBBH2-PwTEqMNLKrrTINwbz0MS9yeRUq5s',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        }),
-      }
-    );
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
+
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBBH2-PwTEqMNLKrrTINwbz0MS9yeRUq5s';
+
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBBH2-PwTEqMNLKrrTINwbz0MS9yeRUq5s';
+    }
+
+    const res = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      }),
+    });
 
     const resData = await res.json();
 
     if (!res.ok) {
       //error
       const error = new Error(resData.error.message || 'Failed');
-      console.log(error);
+
       throw error;
     }
 
-    console.log(resData);
+    const expiresIn = +resData.expiresIn * 1000;
+    // const expiresIn = 5000;
+
+    const expirationDate = new Date().getTime() + expiresIn;
+
+    localStorage.setItem('token', resData.idToken);
+    localStorage.setItem('userId', resData.localId);
+    localStorage.setItem('tokenExpiration', expirationDate);
+
+    timer = setTimeout(() => {
+      context.dispatch('autoLogout');
+    }, expiresIn);
+
     context.commit('setUser', {
       token: resData.idToken,
       userId: resData.localId,
-      tokenExpiration: resData.expiresIn,
     });
+  },
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+
+    const expiresIn = +tokenExpiration - new Date().getTime();
+
+    if (expiresIn < 0) {
+      return;
+    }
+
+    timer = setTimeout(() => {
+      context.dispatch('autoLogout');
+    }, expiresIn);
+
+    if (token && userId) {
+      context.commit('setUser', {
+        token: token,
+        userId: userId,
+      });
+    }
   },
   logout(context) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('tokenExpiration');
+    clearTimeout(timer);
     context.commit('setUser', {
       token: null,
       userId: null,
-      tokenExpiration: null,
     });
+  },
+  autoLogout(context) {
+    context.dispatch('logout');
+    context.commit('setAutoLogout');
   },
 };
